78ab1b2d2d6e633d06d85a05bc4d154b9b8ec62a
Arindam Choudhury <arindam.choudhury.email@gmail.com>
2025-06-27 23:29:15 -0400

    code update

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -1,1 +1,7 @@
-from langgraph 
+from langgraph.graph import StateGraph
+from src.langgraphagenticai.state import State
+
+class GraphBuilder:
+    def __init__(self, model):
+        self.llm = model
+        self.graph_builder=StateGraph(State)
--- src/langgraphagenticai/state/state.py
+++ src/langgraphagenticai/state/state.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel, Field
 from typing_extensions import TypedDict, List
-from langgraph.
-
+from langgraph.graph.message import add_messages
+from typing import Annotated
 
 class State(TypedDict):
     """
     Represent the structure of the state used in graph
     """
-    message: Annotated[List, add_messages]
+    messages: Annotated[List, add_messages]
